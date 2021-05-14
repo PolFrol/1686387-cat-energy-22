f7fe5c3d05560524f69cf258322c620287d5ebc6
Polina <polfroll@gmail.com>
2021-05-14 22:41:31 +0500

    Декоративные элементы страницы формы

--- source/js/header.js
+++ source/js/header.js
@@ -4,11 +4,11 @@ var headerToggle = document.querySelector('.header__toggle');
 navHeader.classList.remove('header--nojs');
 
 headerToggle.addEventListener('click', function() {
-  if (navHeader.classList.contains('.header--closed')) {
-    navHeader.classList.remove('.header--closed');
-    navHeader.classList.add('.header--opened');
+  if (navHeader.classList.contains('header--closed')) {
+    navHeader.classList.remove('header--closed');
+    navHeader.classList.add('header--opened');
   } else {
-    navHeader.classList.add('.header--closed');
-    navHeader.classList.remove('.header--opened');
+    navHeader.classList.add('header--closed');
+    navHeader.classList.remove('header--opened');
   }
 });
