22da06eea64bba9ec240d60d0e91ff415c4e9b8c
Polina <polfroll@gmail.com>
2021-05-22 14:24:49 +0500

    Адаптивная графика для страницы формы правки

--- source/js/input-error.js
+++
@@ -1,8 +0,0 @@
-const btnSubmit = document.querySelector(".container__link");
-const input = document.querySelector(".form__input");
-let iconEmail = document.querySelector(".form__input--mail");
-let iconPhone = document.querySelector(".form__input--phone");
-
-btnSubmit.addEventListener("click", function () {
-
-});
